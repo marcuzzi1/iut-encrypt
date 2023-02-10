d6a9550da138aff39ed8a187086faca0ffd72999
marcuzzi1 <quentin.marcuzzi@etu.unilim.fr>
2023-02-10 16:42:39 +0100

    Added password encoding and verifying functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 'use strict';
 
+const { bcrypt } = require('bcryptjs');
+
 exports.hello = function() {
     return 'Hello World!';
+}
+
+exports.encrypt = async function(password) {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+}
+
+exports.compare = async function(password, receivedPassword) {
+    return await bcrypt.compare(password, receivedPassword);
 }
